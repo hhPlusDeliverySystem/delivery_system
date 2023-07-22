99022828b15b44c22b0a2be864794268b9b711dc
yuseonpark <pyuseon@gmail.com>
2023-07-22 23:15:26 +0900

    feat: add loggerService for log and logging for errors

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ import * as winston from 'winston'
 import { createLogger } from 'winston';
 import DailyRotateFile from 'winston-daily-rotate-file';
 import { winstonLogger } from './utils/winston.util';
+import { HttpExceptionFilter } from './middleware/exception.filter';
 
 async function bootstrap() {
 
@@ -26,7 +27,7 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
+  app.useGlobalFilters(new HttpExceptionFilter);
   await app.listen(3000);
 }
 bootstrap();
